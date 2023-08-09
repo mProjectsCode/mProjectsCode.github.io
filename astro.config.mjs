74107004a1f99796ed96c00fc8da646de581e074
mProjectsCode <m.projects.code@gmail.com>
2023-08-09 22:58:31 +0200

    some clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mprojectscode.github.io/',
+  site: 'https://mprojectscode.github.io/',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })]
 });
---
+++ src/utils/images.ts
@@ -0,0 +1,54 @@
+
+export interface Image {
+	src: string,
+	title: string,
+	subtitle: string,
+}
+
+export const images: Image[] = [
+	{
+		src: '/img/art/white_lines/white_lines_1920x1080.png',
+		title: 'White Lines',
+		subtitle: 'Abstract white lines flowing through red-tinted terrain',
+	},
+	{
+		src: '/img/art/spikes_2/spikes_2_1706x480.png',
+		title: 'Spiky Landscape',
+		subtitle: 'A bizarre spiky and purple landscape',
+	},
+	{
+		src: '/img/art/wave_cube/wave_cube_1706x480.png',
+		title: 'Wave Cube',
+		subtitle: 'A strange alien like cube',
+	},
+	{
+		src: '/img/art/mandelbulb/mandelbulb_1920x1080.png',
+		title: 'Mandelbulb',
+		subtitle: 'A render of the Mandelbulb fractal',
+	},
+	{
+		src: '/img/art/julia_3d/julia_3d_1920x1080.png',
+		title: 'Julia 3D',
+		subtitle: 'A cut open three dimensional Julia fractal',
+	},
+	{
+		src: '/img/art/red_lines/red_lines_1920x1080.png',
+		title: 'Red Lines',
+		subtitle: 'Layered wavy red lines',
+	},
+	{
+		src: '/img/art/floating_hexagons/floating_hexagons_1920x1080.png',
+		title: 'Floating Hexagons',
+		subtitle: 'Mysterious hexagons floating above a bright white plane',
+	},
+	{
+		src: '/img/art/crystal_pillars/crystal_pillars_1215x1080.png',
+		title: 'Crystal Pillars',
+		subtitle: 'Two crystal pillars touching, in light and dark'
+	},
+	{
+		src: '/img/art/vase/vase_1920x1080.png',
+		title: 'Vase',
+		subtitle: 'A modern Vase design',
+	},
+];
